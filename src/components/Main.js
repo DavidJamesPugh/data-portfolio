b444f8b629fceddcd0522787cb71ef23ffc0e1d4
davidjamespugh <davidjames.pugh@gmail.com>
2021-05-08 22:29:35 +1200

    Centering & playing with styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -96,7 +96,8 @@ class Main extends React.Component {
           <p>
             <h3 className="major align-center">Education</h3>
           </p>
-            <h4>Bachelors of Science, Statistics</h4>
+            <h4>Bachelors of Science, Statistics
+            <br />The University of Auckland</h4>
           <p className="small">
             Statistics
             <br/>Computer Science
@@ -105,7 +106,9 @@ class Main extends React.Component {
             <br/>Accounting
           </p>
 
-            <h4>Certificate in Apiculture Level 3</h4>
+            <h4>Certificate in Apiculture Level 3
+            <br />Toi Ohomai Institute of Tecnology
+            </h4>
           <p className="small">
             Hive Inspections
             <br/>Honey Extraction (3 Frame Spinner)
@@ -116,10 +119,6 @@ class Main extends React.Component {
           </p>
           <h3 className="major">Work Experience</h3>
 
-          <h4 className="align-center">Wilderland Organics (March 2020 - April 2021)
-            <br/>
-            Planning, People Team Leader, Financial Manager
-          </h4>
           <h4 className="align-center">Wilderland Organics (March 2020 - April 2021)
             <br/>
             Planning, People Team Leader, Financial Manager
@@ -135,6 +134,15 @@ class Main extends React.Component {
               <li>
                 Making and reporting of financial decisions to facilitate the functioning of Wilderland
               </li>
+              <li>
+                Developing policy for how to approach interpersonal issues in the community
+              </li>
+              <li>
+                Fostering a healthy, collaborative culture that enables high quality work to be done
+              </li>
+              <li>
+                Establish new strategies and systems in conjunction with the executive team for data management and access.
+              </li>
             </ul>
           </p>
 
@@ -144,14 +152,22 @@ class Main extends React.Component {
           </h4>
           <p>
             <ul>
-            <li>
+              <li>
               Using Excel, Python, Power BI, and R to create databases, data visualisations, statistical models, and
               time series to present an in depth summary of our healthcare outcomes to the Executive Team weekly
-            </li>
-            <li>
+              </li>
+              <li>
               Manage the supply, costings, installation, training, and management of new IT infrastructure on site
-            </li>
-          </ul>
+              </li>
+              <li>Assessing, prioritising and providing support to all staff and residents with
+                IT issues and queries.
+              </li>
+              <li>
+                Writing of extensive technical documentation
+              </li>
+              <li>Provide quarterly run chart summaries showing industry targets, trends,
+                variation and intervention time points</li>
+            </ul>
           </p>
 
 
