1a47701645a22a63eeda2dd100a919ba1801c942
davidjamespugh <davidjames.pugh@gmail.com>
2021-05-12 01:28:04 +1200

    Making a project and putting a leaflet map there

--- src/pages/main_projects.js
+++ src/pages/main_projects.js
@@ -6,10 +6,12 @@ import Layout from '../components/layout'
 const SecondPage = () => (
   <Layout>
     <h1>Investigating Covid using Shiny</h1>
-      <iframe height="400" width="400" frameBorder="0"
+      <iframe seamless height="800" width="400" frameBorder="0"
               src="covid_map.html"></iframe>
-      <iframe height="400" width="400" frameBorder="0"
+      <br />
+      <iframe height="400" width="100%" frameBorder="0"
               src="https://davidjamespugh.shinyapps.io/nzcovidcases/"></iframe>
+      <br />
     <Link to="/">Go back to the homepage</Link>
 
   </Layout>
