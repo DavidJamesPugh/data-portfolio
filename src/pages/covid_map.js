2201c8ed2ba1683ee3060845e1b782849181b2a6
davidjamespugh <davidjames.pugh@gmail.com>
2021-05-30 19:04:55 +1200

    Checking page formatting syntax

--- src/pages/covid_map.js
+++ src/pages/covid_map.js
@@ -12,13 +12,7 @@ const ThirdPage = () => (
       <br />
       <iframe src="https://davidjamespugh.shinyapps.io/nzcovidcases/"></iframe>
       <br />
-      <h1>Investigating Wellington rents with R and Python</h1>
-       <img src={wellington_main} alt={"wellington region"} height="400px" width="400"/>
-      <br/>
-       <img src={wellington_district} alt={"wellington districts"} height="400px" width="400"/>
-      <br/>
-       <img src={wellington_suburb} alt={"wellington suburbs"} height="400px" width="400"/>
-      <br/>
+
     <Link to="/">Go back to the homepage</Link>
 
   </Layout>
